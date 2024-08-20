a6acd609fd3818b3fcbd133464d39334017d08a0
gisel gauna <gisellgauna@hotmail.com.ar>
2024-08-19 23:23:10 -0300

    entregablegauna

---
+++ JS/main.js
@@ -0,0 +1,53 @@
+//const productos = ["almacen", "chocolates", "fiambreria", "bebidas", "helados"]
+//console.log(productos)
+
+
+const producto1 = {
+
+    caracteristicas: "",
+    tipo: "", 
+    precio: "",
+    descuento: 10
+}
+const producto2 = {
+
+    caracteristicas: "",
+    tipo: "", 
+    precio: "",
+    descuento: 10
+}
+const producto3 = {
+
+    caracteristicas: "",
+    tipo: "", 
+    precio: "",
+    descuento: 10
+}
+const producto4 = {
+
+    caracteristicas: "",
+    tipo: "", 
+    precio: "",
+    descuento: 10
+}
+const producto5 = {
+
+    caracteristicas: "",
+    tipo: "", 
+    precio: "",
+    descuento: 10
+}
+const producto6 = {
+
+    caracteristicas: "",
+    tipo: "", 
+    precio: "",
+    descuento: 10
+}
+const producto7 = {
+
+    caracteristicas: "",
+    tipo: "", 
+    precio: "",
+    descuento: 10
+}
